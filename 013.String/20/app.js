29c4bc571dbe01ca49fd9956b9089e32b2478cb4
AntonMiranovich <antonmir992@gmail.com>
2023-07-02 16:08:51 +0300

    added tasks 013.1-20

--- 013.String/20/app.js
+++ 013.String/20/app.js
@@ -1,3 +1,12 @@
+// Пользователь вводит строку. Необходимо посчитать количество гласных
 
+const a = prompt().trim().toLowerCase();
+let b = 0
+for (let i = 0; i < a.length; i++) {
+    if (a[i] === 'a' || a[i] === 'i' || a[i] === 'o' || a[i] === 'e' || a[i] === 'u') {
+        b += 1
+    };
+}
+console.log(b);
 
 
