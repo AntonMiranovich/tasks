d2f4eb2194b49b1e10b892ae196b2b9e4931d522
AntonMiranovich <antonmir992@gmail.com>
2023-07-02 16:01:58 +0300

    added tasks 013.1-18

--- 013.String/14/app.js
+++ 013.String/14/app.js
@@ -1,3 +1,12 @@
+// На вход подается url. Если он начинается с http, содержит хотя бы один “/” и
+// заканчивается подстрокой .com или .ru, то вывести true, в противном случае false
 
 
+let url = prompt('ВВедите адрес');
+
+if (url[0].includes('http') && url.includes('/') && url.includes('.ru') || url.includes('.com')) {
+    console.log(true);
+} else {
+    console.log(false);
+}
 
--- 013.String/15/app.js
+++ 013.String/15/app.js
@@ -1,3 +1,8 @@
+// Дан массив ['я', 'учу', 'javascript', '!']. С помощью метода join преобразуйте массив в
+// строку 'я+учу+javascript+!.
 
 
+let a = ['я', 'учу', 'javascript', '!'].join()
+
+console.log(a.replaceAll(',', '+'));
 
