602693e809aa683eb11983d26b2e25ca10c65465
AntonMiranovich <antonmir992@gmail.com>
2023-07-02 14:44:44 +0300

    added tasks 012

--- 012.Ternarny/14/app.js
+++ 012.Ternarny/14/app.js
@@ -2,3 +2,13 @@
 // Добавить проверки для строк. Удалить лишние пробелы
 
 
+let a = prompt('ВВедите название').trim;
+
+if (isNaN(a)) {
+    console.log(a == 'hschool' ? 'true' : 'false');
+} else {
+    console.log('Ошибка ввода');
+}
+
+
+
