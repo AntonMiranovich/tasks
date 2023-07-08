7096d0111bcd1ec2f2a061492b7735fef07c1112
AntonMiranovich <antonmir992@gmail.com>
2023-07-08 19:11:47 +0300

    work 15/1-18

--- 015.For/17/app.js
+++ 015.For/17/app.js
@@ -1,8 +1,16 @@
 // Дан массив с элементами 1, 2, 3, 4, 5, 6, 7, 8, 9. С помощью цикла for создайте строку
 // '-1-2-3-4-5-6-7-8-9-‘
 
-let arr=[1, 2, 3, 4, 5, 6, 7, 8, 9].join('').replaceAll('','-');
+//Вариант 1:
 
-
-
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9].join('').replaceAll('', '-');
 console.log(arr);
+
+//Вариант 2:
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let result = ''
+for (let i = 0; i < array.length; i++) {
+    result += '-' + array[i];
+}
+console.log(result + '-')
