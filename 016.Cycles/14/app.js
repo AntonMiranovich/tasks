f179cc5ed0e8c9ab5831e52bcbe9c8139bb91f34
Anton Miranovich <antonmir992@gmail.com>
2023-07-17 19:55:47 +0300

    work 16/changes ;

--- 016.Cycles/14/app.js
+++ 016.Cycles/14/app.js
@@ -2,12 +2,12 @@
 // те числа из массива, которые начинаются на цифру 1, 2 или 5 -> 10, 20, 50, 235
 
 
-const arr = [10, 20, 30, 50, 235, 3000]
-let new_arr = []
+const arr = [10, 20, 30, 50, 235, 3000];
+let new_arr = [];
 
 for (let i = 0; i < arr.length; i++) {
-    arr[i] = String(arr[i])
-    arr[i][0] == 1 || arr[i][0] == 2 || arr[i][0] == 5 ? new_arr.push(Number(arr[i])) : null
+    arr[i] = String(arr[i]);
+    arr[i][0] == 1 || arr[i][0] == 2 || arr[i][0] == 5 ? new_arr.push(Number(arr[i])) : null;
 }
 
 console.log(new_arr);
