42543ac35d73c6ab15d1f06ff77c9c056fbcfef0
Anton Miranovich <antonmir992@gmail.com>
2023-07-20 15:25:11 +0300

    work 17-5

--- 017.Arr_methods/4/app.js
+++ 017.Arr_methods/4/app.js
@@ -1,2 +1,18 @@
 // Заполнять массив числами бесконечно до тех пор, пока длина массива не будет
 // равна 5. Вывести массив в консоль (цикл for, while)
+
+const n = 5;
+arr = [];
+
+// for (let i = 0; i < n; i++) {
+//   arr.push(i);
+// }
+
+// console.log(arr);
+
+let i = 0;
+while (i < n) {
+  arr.push(i);
+  i++;
+}
+console.log(arr);
--- 017.Arr_methods/5/app.js
+++ 017.Arr_methods/5/app.js
@@ -1,3 +1,16 @@
 // На входе 10 элементов. Из элементов составить 2 массива по 5 элементов. Далее
 // необходимо сравнить массивы. Если они идентичны, то вывести булевое true, в
 // противном случае false
+
+let arr = [];
+let arr2 = [];
+
+for (let i = 1; i <= 5; i++) {
+  arr.push(prompt());
+}
+
+for (let i = 1; i <= 5; i++) {
+  arr2.push(prompt());
+}
+
+arr.join('') === arr2.join('') ? console.log(true) : console.log(false);
