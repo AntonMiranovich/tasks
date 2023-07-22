235dba0783ff8fee52f606f16ee78fca204fb3fa
Anton Miranovich <antonmir992@gmail.com>
2023-07-22 22:09:14 +0300

    work 17-20/1

--- 017.Arr_methods/16/app.js
+++ 017.Arr_methods/16/app.js
@@ -11,26 +11,48 @@
 // 6 -> [[1, 2, 3, 4, 5, 6]]
 
 const arr = [1, 2, 3, 4, 5, 6];
-const n = +prompt('введиче число');
+const n = 4
+// +prompt('введиче число');
 
-if (!isNaN(n) && n <= arr.length && n !== 0 && n !== "") {
-  let arrResult = [];
-  let count = 1;
+
+//Вариант 1
+// if (!isNaN(n) && n <= arr.length && n !== 0 && n !== "") {
+//   let arrResult = [];
+//   let count = 1;
+//   let temporaryArr = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (count === n) {
+//       temporaryArr.push([arr[i]]);
+//       arrResult.push(temporaryArr);
+//       count = 1;
+//       temporaryArr = [];
+//     } else {
+//       temporaryArr.push([arr[i]]);
+//       count++;
+//     }
+//   }
+//   temporaryArr.length === 0 ? null : arrResult.push(temporaryArr);
+//   console.log(arrResult);
+// } else {
+//   console.log("Введите корректное значение");
+// }
+
+
+//Вариант 2
+if (!isNaN(n)) {
+  let result = [];
   let temporaryArr = [];
-
   for (let i = 0; i < arr.length; i++) {
-    if (count === n) {
-      temporaryArr.push([arr[i]]);
-      arrResult.push(temporaryArr);
-      count = 1;
-      temporaryArr = [];
-    } else {
-      temporaryArr.push([arr[i]]);
-      count++;
-    }
+    temporaryArr.push(arr[i])
+
+      if (temporaryArr.length === n) {
+          result.push(temporaryArr);
+          temporaryArr = [];
+      }
   }
-  temporaryArr.length === 0 ? null : arrResult.push(temporaryArr);
-  console.log(arrResult);
+  temporaryArr.length === 0 ? null : result.push(temporaryArr);
+  console.log(result);
 } else {
-  console.log("Введите корректное значение");
+  console.log('ошибка ввода');
 }
