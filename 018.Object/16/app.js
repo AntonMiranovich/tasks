018e6d95e953d1733a36ba36b348a4fffe401592
Anton Miranovich <antonmir992@gmail.com>
2023-07-24 15:49:35 +0300

    work 18-18

--- 018.Object/16/app.js
+++ 018.Object/16/app.js
@@ -1,2 +1,22 @@
 // 16. На входе объект из 5 свойств. Значения вводим с клавиатуры. Необходимо
 // проитерировать значения объекта и сформировать на основе этих чисел массив
+
+const obj = {
+  name: "",
+  age: "",
+  weight: "",
+  height: "",
+  shoeSize: "",
+};
+
+for (let key in obj) {
+  obj[key] = prompt(`Введите ${key}`);
+}
+
+const arr = [];
+for (let key in obj) {
+  !isNaN(obj[key]) ? arr.push(obj[key]) : null;
+}
+
+console.log(obj);
+console.log(arr);
--- 018.Object/17/app.js
+++ 018.Object/17/app.js
@@ -3,12 +3,12 @@
 const arr = [1, 1, 1, 2, 3, 4, 4, 4, 3, 2, 5, 6, 7, 6, 6, 5, 4, 1, 1, 1];
 let obj = {};
 
+//Есди есть то добовляет к эл+1
 for (let el of arr) {
   obj[el] ? obj[el] += 1 : obj[el] = 1;
 }
 
 const repetition = [];
-
 for (let key in obj) {
     repetition.push(obj[key]);
 }
