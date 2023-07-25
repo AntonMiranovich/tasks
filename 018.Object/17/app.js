406f9ccf553476f31d4417457185f65f7355b9f1
Anton Miranovich <antonmir992@gmail.com>
2023-07-25 20:19:55 +0300

    work 18-18.1

--- 018.Object/17/app.js
+++ 018.Object/17/app.js
@@ -3,7 +3,6 @@
 const arr = [1, 1, 1, 2, 3, 4, 4, 4, 3, 2, 5, 6, 7, 6, 6, 5, 4, 1, 1, 1];
 let obj = {};
 
-//Есди есть то добовляет к эл+1
 for (let el of arr) {
   obj[el] ? obj[el] += 1 : obj[el] = 1;
 }
@@ -15,7 +14,6 @@ for (let key in obj) {
 
 const maxNum = Math.max(...repetition);
 
-//for для того чтобы знать какое значение повторяется чаще 
 for (let key in obj) {
   obj[key] == maxNum ?
      console.log(`Значение массива ${key} повторяющееся в нем наибольшее количество раз,колличество повторений ${obj[key]} раз`): null;
