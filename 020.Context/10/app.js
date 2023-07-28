740cf989613f8ede1d76a6cdd479ad81f6e71640
Anton Miranovich <antonmir992@gmail.com>
2023-07-28 20:34:55 +0300

    work 20-5

--- 020.Context/10/app.js
+++ 020.Context/10/app.js
@@ -1,3 +1,21 @@
 // 10. Напишите функцию, которая принимает два параметра: предложение из
 // нескольких слов и букву. Функция должна подсчитывать количество вхождений
 // указанной буквы в строке. Добавить проверки
+
+const str1 = "hello , i am anton";
+const str2 = "a";
+
+function checkStr(str1, str2) {
+  let check = 0;
+  if (isNaN(str1) && isNaN(str2)) {
+    for (let i = 0; i < str1.length; i++) {
+      str1[i] == str2 ? (check += 1) : null;
+    }
+  }else{
+    console.log('значение не слова или буквы');
+  }
+  return check
+}
+
+const result = checkStr(str1, str2);
+console.log(`количество вхождений указанной буквы в строке ровна ${result}`);
