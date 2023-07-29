01b8be099ff9820a715dc4bded77c68e4573102e
Anton Miranovich <antonmir992@gmail.com>
2023-07-29 19:46:37 +0300

    work 20-9

--- 020.Context/11/app.js
+++ 020.Context/11/app.js
@@ -1,5 +1,19 @@
 // 11. Напишите функцию, принимающую в качестве параметра статичный объект.
 // Функция возвращает новый объект, где значения – исключительно числа
 // первоначального объекта. IIFE
+const obj = {
+  id: 1,
+  name: "Anton",
+  age: 30,
+};
+
+
+(function doValue(a) {
+  let newObj = {};
+  for(let key in a){
+    !isNaN(a[key])?newObj[key]=a[key]:null
+  }
+  console.log(newObj);
+}(obj))
 
 
