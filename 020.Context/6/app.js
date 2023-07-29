70d39114ee926d782a06105cd43096ac6a4404ae
Anton Miranovich <antonmir992@gmail.com>
2023-07-29 18:39:53 +0300

    work 20-6

--- 020.Context/6/app.js
+++ 020.Context/6/app.js
@@ -1,2 +1,16 @@
 // 6. Работа с замыканием. Реализуйте функцию, которая будет считать количество
 // своих вызовов
+
+function check() {
+  let count = 0;
+  return function check2() {
+    count += 1;
+    console.log(count);
+  };
+}
+
+let countResult = check();
+
+countResult();
+countResult();
+countResult();
--- 020.Context/7/app.js
+++ 020.Context/7/app.js
@@ -1,2 +1,16 @@
 // 7. Работа с замыканием. Необходимо реализовать конкатенацию строк при каждом
 // последующем вызове функции
+
+function check() {
+    let count = '';
+    return function check2() {
+      count += 'Anton ';
+      console.log(count);
+    };
+  }
+  
+  let countResult = check();
+  
+  countResult();
+  countResult();
+  countResult();
