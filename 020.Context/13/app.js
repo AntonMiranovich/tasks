26ebf8c438e8dede155f8c0f9479a3d0b73d95f6
Anton Miranovich <antonmir992@gmail.com>
2023-07-30 14:25:22 +0300

    work 20-16

--- 020.Context/13/app.js
+++ 020.Context/13/app.js
@@ -1,2 +1,16 @@
 // 13. Напишите функцию, принимающую в качестве параметра статичный объект.
 // Функция возвращает количество пар ключ / значение. IIFE
+
+const obj = {
+  name: "Anton",
+  age: 30,
+  id: 1,
+};
+
+(function checkValum(a) {
+  count = 0;
+  for (let key in a) {
+    a[key] !== "" ? (count += 1) : null;
+  }
+  console.log(count);
+})(obj);
