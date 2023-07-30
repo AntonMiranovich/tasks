b826b2cb1f8b64fa532596412423e1189fb912ee
Anton Miranovich <antonmir992@gmail.com>
2023-07-30 13:42:28 +0300

    work 20-11

--- 020.Context/12/app.js
+++ 020.Context/12/app.js
@@ -1,2 +1,18 @@
 // 12. Работа с замыканием. Напишите функцию, каждый вызов который будет
 // генерировать случайные числа от 1 до 100.
+
+function check() {
+    let count = 0;
+    return function check2() {
+      count = Math.floor(Math.random() * 100);
+      console.log(count);
+    };
+  }
+  
+  let countResult = check();
+  
+  countResult();
+  countResult();
+  countResult();
+
+
