5c94e06ab65ee57820a1490a107d9b7585438347
Anton Miranovich <antonmir992@gmail.com>
2023-08-09 21:46:18 +0300

    work 022-1

--- 022.Error/8/app.js
+++ 022.Error/8/app.js
@@ -3,9 +3,43 @@
 // ["a","b","c","d","f"] -> "e"
 // ["O","Q","R","S"] -> "P"
 
-const arr = ["a", "b", "c", "d", "f"].join("").toLowerCase();
+const arr = ["O", "Q", "R", "S"].join("").toLowerCase();
 
+// Вариант если пропавших букв больше чем одна 
 function checkAlp(arr) {
+  try {
+    for (let i = 0; i < arr.length; i++) {
+      if (!isNaN(arr[i])) throw new Error("В массиве присутствуют числа");
+    }
+  } catch (error) {
+    return error.message;
+  }
+
+  const alp = "abcdefghijklmnopqrstuvwxyz";
+  const firstLetter = arr[0];
+  const lastLetter = arr[arr.length - 1];
+  const partAlp = alp.slice(
+    alp.indexOf(firstLetter),
+    alp.lastIndexOf(lastLetter)
+  );
+  let res = "";
+  let arr2 = arr;//новый временный массив с заглушками 
+  for (let i = 0; i < partAlp.length; i++) {
+    if (partAlp[i] !== arr2[i]) {
+      res += partAlp[i]+' ';
+      arr2 = arr2.slice(arr2[0], arr2[i]) + "2" + arr2.slice(arr2[i]);// "2" это заглушка чтобы индексы совпадали
+    }
+  }
+  return res;
+}
+
+let result = checkAlp(arr);
+console.log(result);
+
+
+
+//Вариант с одной пропавшей буквой
+// function checkAlp(arr) {
 //   try {
 //     for (let i = 0; i < arr.length; i++) {
 //       if (!isNaN(arr[i])) throw new Error("В массиве присутствуют числа");
@@ -14,16 +48,22 @@ function checkAlp(arr) {
 //     return error.message;
 //   }
 
-  const alp = "abcdefghijklmnopqrstuvwxyz";
-  const firstNum = arr[0];
-  const lastNum = arr[arr.length - 1];
-  const partAlp = alp.slice(firstNum, lastNum);
-  let res = "";
-  for (let i = 0; i < partAlp.length; i++) {
-    if (partAlp[i] !== arr[i]) res += partAlp[i];
-  }
-  return res;
-}
+//   const alp = "abcdefghijklmnopqrstuvwxyz";
+//   const firstLetter = arr[0];
+//   const lastLetter = arr[arr.length - 1];
+//   const partAlp = alp.slice(
+//     alp.indexOf(firstLetter),
+//     alp.lastIndexOf(lastLetter)
+//   );
+//   let res = "";
+//   for (let i = 0; i < partAlp.length; i++) {
+//     if (partAlp[i] !== arr[i]) {
+//       res += partAlp[i];
+//       break;
+//     }
+//   }
+//   return res;
+// }
 
-let result = checkAlp(arr);
-console.log(result);
+// let result = checkAlp(arr);
+// console.log(result);
