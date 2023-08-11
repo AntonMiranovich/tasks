717cc1b55eb61ffc1b6d6194c537841a41e22b4d
Anton Miranovich <antonmir992@gmail.com>
2023-08-11 20:32:07 +0300

    work 023-1

--- 022.Error/8.1/app.js
+++ 022.Error/8.1/app.js
@@ -2,6 +2,7 @@
 // (возрастающих) букв и возвращает отсутствующую в массиве. Добавить проверки
 // ["a","b","c","d","f"] -> "e"
 // ["O","Q","R","S"] -> "P"
+// (Вариант если пропавших букв больше чем одна)
 
 const arr = ["O", "Q", "R", "S"].join("").toLowerCase();
 
@@ -10,25 +11,26 @@ function checkAlp(arr) {
     for (let i = 0; i < arr.length; i++) {
       if (!isNaN(arr[i])) throw new Error("В массиве присутствуют числа");
     }
+
+    const alp = "abcdefghijklmnopqrstuvwxyz";
+    const firstLetter = arr[0];
+    const lastLetter = arr[arr.length - 1];
+    const partAlp = alp.slice(
+      alp.indexOf(firstLetter),
+      alp.lastIndexOf(lastLetter)
+    );
+    let res = "";
+    let arr2 = arr;
+    for (let i = 0; i < partAlp.length; i++) {
+      if (partAlp[i] !== arr2[i]) {
+        res += partAlp[i] + " ";
+        arr2 = arr2.slice(arr2[0], arr2[i]) + "1" + arr2.slice(arr2[i]);
+      }
+    }
+    return res;
   } catch (error) {
     return error.message;
   }
-
-  const alp = "abcdefghijklmnopqrstuvwxyz";
-  const firstLetter = arr[0];
-  const lastLetter = arr[arr.length - 1];
-  const partAlp = alp.slice(
-    alp.indexOf(firstLetter),
-    alp.lastIndexOf(lastLetter)
-  );
-  let res = "";
-  for (let i = 0; i < partAlp.length; i++) {
-    if (partAlp[i] !== arr[i]) {
-      res += partAlp[i];
-      break;
-    }
-  }
-  return res;
 }
 
 let result = checkAlp(arr);
