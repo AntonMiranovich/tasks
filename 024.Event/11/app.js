e3f32a62a646e67379aca630d87adde7d67c3ebe
Anton Miranovich <antonmir992@gmail.com>
2023-08-16 19:30:21 +0300

    work 022-12 fix

--- 024.Event/11/app.js
+++ 024.Event/11/app.js
@@ -5,8 +5,10 @@ const btn = document.querySelector("button");
 btn.addEventListener("click", function () {
   const inp1 = document.querySelector(".inp1");
   const inp2 = document.querySelector(".inp2");
+
   let tempInp1 = inp1.value;
   let tempInp2 = inp2.value;
+  
   inp1.value = tempInp2;
   inp2.value = tempInp1;
 });
