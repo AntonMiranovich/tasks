76e41d59017a0563294b674537a939319e3ceb12
Anton Miranovich <antonmir992@gmail.com>
2023-08-25 21:05:51 +0300

    tasks 027.10

--- 027.Prototype/10/app.js
+++ 027.Prototype/10/app.js
@@ -3,3 +3,22 @@
 // кодом проделанной работы за спринт. Программисту необходимо вырезать все
 // комментарии в коде новым коммитом. Задача: вырезать куски кода комментариев
 // используя регулярные выражения воизбежание публичного стыда.
+
+const str = `
+const a=20
+const b=30
+/* adwdawdwewqe asdaqw 
+dsd12123@#$ sdad*&
+EWRWERdsasdqw qweq */
+const c=a+b
+`;
+
+function delCom(str) {
+  const clearJsCode = str.replaceAll(
+    /^\/\*[A-z0-9\/\*\!\@\#\$\%\^\&\(\) \-\.\n]+$/gm,
+    ""
+  );
+  return clearJsCode;
+}
+
+console.log(delCom(str));
--- 027.Prototype/9/app.js
+++ 027.Prototype/9/app.js
@@ -1,4 +1,21 @@
 // 9. На входе объект. Ключи и значения - автоинкремент (генерируется автоматически
 //     от 1 до n). Необходимо вывести количество пар ключ значение объекта)
-    
-    
+
+const obj = {
+  id: 12345,
+  name: "Anton",
+  surname: "Miranovich",
+  age: 30
+};
+
+function sumKey(obj) {
+  let check = 0;
+  for (key in obj) {
+    if (obj.key !== "") {
+      check++;
+    }
+  }
+  return check
+}
+
+console.log(sumKey(obj));
