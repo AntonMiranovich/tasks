f4055523a1e02c8460e361be97c7e735ac9c810e
Anton Miranovich <antonmir992@gmail.com>
2023-08-25 21:34:44 +0300

    tasks 027.13

--- 027.Prototype/11/app.js
+++ 027.Prototype/11/app.js
@@ -1,3 +1,21 @@
 // 11. Напишите функцию, принимающую в качестве параметра статичный объект.
 // Функция возвращает новый объект, где значения – исключительно числа
 // первоначального объекта. IIFE
+
+const obj = {
+  id: 12345,
+  name: 'anton',
+  age: 30,
+  email: 'anton@mail.ru'
+};
+
+
+ (function (object) {
+    let newObj = {};
+    for (const key in object) {
+      if (!isNaN(object[key])) {
+        newObj[key] = object[key];
+      }
+    }
+    console.log(newObj);
+  }(obj));
--- 027.Prototype/12/app.js
+++ 027.Prototype/12/app.js
@@ -1,1 +1,12 @@
 // 12. Реализуйте функцию, которая будет считать количество своих вызовов
+
+let check=0
+
+
+function sumCheck(){
+    console.log(++check);
+}
+
+sumCheck()
+sumCheck()
+sumCheck()
