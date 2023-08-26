cd02a51e0c2e90e1ce64cafdf14c035c9cb75fc7
Anton Miranovich <antonmir992@gmail.com>
2023-08-26 11:47:39 +0300

    tasks 027.14

--- 027.Prototype/13/app.js
+++ 027.Prototype/13/app.js
@@ -1,1 +1,10 @@
 // 13. Напишите алгоритм для нахождения факториала числа
+
+const num=10
+
+function doFact(n){
+if(n<=1) return 1
+return n*doFact(n-1)
+}
+
+console.log(doFact(num));
--- 027.Prototype/14/app.js
+++ 027.Prototype/14/app.js
@@ -3,3 +3,17 @@
 // предложение VAPORWAVE преобразует все буквы в верхний регистр и добавляет
 // 2 пробела между каждой буквой (или специальным символом)
 // Why isn't my code working? – W H Y I S N ' T M Y C O D E W O R K I N G ?
+
+const str = `Why isn't my code working?`;
+
+(function vaporStr(str) {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] != " ") {
+      result += str[i].toUpperCase() + "  ";
+    }
+  }
+
+  console.log(result);
+})(str);
