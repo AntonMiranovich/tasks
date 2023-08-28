448f46a18b57a8c1ebe44852eb0e578db2e59b0b
Anton Miranovich <antonmir992@gmail.com>
2023-08-28 17:49:46 +0300

    tasks 028

--- 028.OOP/5/app.js
+++ 028.OOP/5/app.js
@@ -1,6 +1,33 @@
-// 5. Реализуйте класс Anagram (Анаграмма это слово или фраза , 
+// 5. Реализуйте класс Anagram (Анаграмма это слово или фраза ,
 //     полученный путем
-//     замены букв исходного слова или фразы). 
+//     замены букв исходного слова или фразы).
 //     Создать функцию для вывода ряда true,
 //     если слова являются анаграммами.
 //      Добавить проверки на ввод
+
+class Anagram {
+  str1;
+  str2;
+
+  isValid() {
+    if (!isNaN(this.str1 || this.str2)) throw new Error("Ошибка ввода");
+  }
+
+  doCheck() {
+    try {
+        this.isValid()
+      if (this.str1.split("").sort().join("") === this.str2.split("").sort().join("")) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      return error.message;
+    }
+  }
+}
+
+const anagram = new Anagram();
+anagram.str1 = "пила";
+anagram.str2 = 'липа';
+console.log(anagram.doCheck());
