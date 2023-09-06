ec42908271312eb134680d43912d99db8b783a2e
Anton Miranovich <antonmir992@gmail.com>
2023-09-06 19:22:28 +0300

    tasks 6-7

--- 031.Polymorphism/6/app.js
+++ 031.Polymorphism/6/app.js
@@ -2,3 +2,23 @@
 // данные из 2 инпутов: почта и пароль и по клику на кнопку «отправить запрос на
 // сервер» (отправку запроса мы не проходили => просто вывести на экран
 // образовавщийся объект вида: {email: email, pwd: pwd}
+
+class Client {
+  sendRequest() {
+    const btn = document.querySelector(".doBut");
+    btn.addEventListener("click", function () {
+      const inpEmail = document.querySelector(".email");
+      const inpPassword = document.querySelector(".password");
+      const result = document.querySelector("div");
+      const objRes = {};
+      objRes.email = inpEmail.value;
+      objRes.pwd = inpPassword.value;
+      result.innerHTML = JSON.stringify(objRes);
+      inpEmail.value = "";
+      inpPassword.value = "";
+    });
+  }
+}
+
+const client = new Client();
+client.sendRequest();
--- 031.Polymorphism/6/appTasks7.js
+++ 031.Polymorphism/6/appTasks7.js
@@ -10,3 +10,40 @@
 // repository – функция, симулирующая БД. Хранит массив данных. Взаимодействие с
 // этим массивом осуществляется только в repository. Массив находится в
 // приложении
+
+class Server {
+  controller(bd) {
+    console.log(bd);
+  }
+
+  service(bd) {
+    this.controller(bd);
+  }
+
+  repository() {
+    const bd = [];
+
+    const btnPush = document.querySelector(".push");
+    btnPush.addEventListener("click", () => {
+      try {
+        const objJson = JSON.parse(document.querySelector("div").textContent);
+        if (objJson.email == "" || objJson.pwd == "")
+          throw new Error("Вы неможите добавить в Баззу данных пустой обьект");
+
+        for (let i = 0; i < bd.length; i++) {
+          if (bd[i].email === objJson.email)
+            throw new Error("В базе данных уже есть такой Email");
+        }
+
+        bd.push({ id: bd.length + 1, ...objJson });
+
+        this.service(bd);
+      } catch (error) {
+        alert(error.message);
+      }
+    });
+  }
+}
+
+const server = new Server();
+server.repository();
