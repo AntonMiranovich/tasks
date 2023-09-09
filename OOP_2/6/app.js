579bb9826cdcbc43525ddde6cf13a998b07c50c9
Anton Miranovich <antonmir992@gmail.com>
2023-09-09 16:52:29 +0300

    tasks oop266

--- OOP_2/6/app.js
+++ OOP_2/6/app.js
@@ -2,3 +2,18 @@
 //  инпут в виде массива. Добавить
 // проверку, что поле не пустое
 
+class DoArr {
+  doArrOnClick() {
+    const btn = document.querySelector("button");
+    const arr = [];
+    btn.addEventListener("click", function () {
+      const inp = document.querySelector("input").value;
+      const div = document.querySelector("div");
+      arr.push(inp);
+      div.innerHTML = arr;
+    });
+  }
+}
+
+const doArr = new DoArr();
+doArr.doArrOnClick();
