e5d33000b73728ff94f670953a0e3ef455ea9ba7
Anton Miranovich <antonmir992@gmail.com>
2023-09-13 22:59:45 +0300

    tasks

--- 032.OOP_2/9/app.js
+++ 032.OOP_2/9/app.js
@@ -2,24 +2,4 @@
 // распознать по какому из значений контейнера производится событие клика. Если
 // клик происходит, то удалить данное значение из массива в div
 
-class DelitElArr {
-  doArr() {
-    const div = document.querySelector("div");
-    const arr = [];
-    for (let i = 0; i < 15; i++) {
-      arr.push(Math.floor(Math.random() * 100));
-    }
-    div.innerHTML = arr;
-  }
 
-  delEl() {
-    const div = document.querySelector("div");
-    div.addEventListener("click", function (event) {
-      event.target.remove();
-    });
-  }
-}
-
-const delitElArr = new DelitElArr();
-delitElArr.doArr();
-delitElArr.delEl();
