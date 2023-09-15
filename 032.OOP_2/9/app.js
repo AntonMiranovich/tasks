d0af5e329ff0c7c161728cde106e77902a772775
Anton Miranovich <antonmir992@gmail.com>
2023-09-15 18:15:54 +0300

    tasks09

--- 032.OOP_2/9/app.js
+++ 032.OOP_2/9/app.js
@@ -2,4 +2,14 @@
 // распознать по какому из значений контейнера производится событие клика. Если
 // клик происходит, то удалить данное значение из массива в div
 
+const div = document.querySelector("div");
 
+for (let i = 1; i <= 15; i++) {
+  let p = document.createElement("p");
+  p.textContent = Math.floor(Math.random() * 100);
+  div.appendChild(p);
+}
+
+div.addEventListener("click", function (event) {
+  event.target.remove();
+});
