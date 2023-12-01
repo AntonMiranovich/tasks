fef0b2ad56b378e2f0b779322f1e873abfdb13e2
AntonMiranovich <antonmir992@gmail.com>
2023-12-01 22:09:43 +0300

    rework tasks2

--- 016.Cycles/7/app.js
+++ 016.Cycles/7/app.js
@@ -5,27 +5,25 @@ const a = [1, 2, 3, 4, 5];
 
 let result = 0;
 
-
-
-
-for (let i = 0; i < a.length; i++) {
-  result += a[i];
-}
-console.log(result);
-
-
-
-
 for (let el of a) {
   result += el;
 }
 
-let i = 0;
-
-
-
-do {
-  result += a[i];
-  i++;
-} while (i < a.length);
 console.log(result);
+
+// for (let i = 0; i < a.length; i++) {
+//   result += a[i];
+// }
+
+
+// let i = 0;
+
+// do {
+//   result += a[i];
+//   i++;
+// } while (i < a.length);
+// console.log(result);
+
+// const result = a.reduce((sum, el) => (sum += el), 0);
+
+// console.log(result);
