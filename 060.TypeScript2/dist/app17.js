484cb09d3966caaf9ef459512d7c30d8ff7c8a10
AntonMiranovich <antonmir992@gmail.com>
2023-12-11 17:32:47 +0300

    tasks 1

--- 060.TypeScript2/dist/app17.js
+++ 060.TypeScript2/dist/app17.js
@@ -1,16 +1,10 @@
 // 17. Создайте массив чисел и найдите наибольшую разницу между двумя элементами.
-const arr17 = [102, 4, 51, 7, 45, 46];
+const arr17 = [102, 51, 7, 45, 2, 46];
 let res17 = 0;
 for (let i = 0; i < arr17.length; i++) {
     for (let j = 0; j < arr17.length; j++) {
-        if (arr17[i] - arr17[j] > res17 || arr17[j] - arr17[i] > res17) {
-            if (arr17[i] > arr17[j]) {
-                res17 = arr17[i] - arr17[j];
-            }
-            else {
-                res17 = arr17[j] - arr17[i];
-            }
-        }
+        if (arr17[i] - arr17[j] > res17)
+            res17 = arr17[i] - arr17[j];
     }
 }
 console.log(`наибольшая разницу между двумя элементами ${res17}`);
