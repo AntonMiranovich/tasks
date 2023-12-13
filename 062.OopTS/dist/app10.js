33ab11a5ea2b99df0e43fe7f332ba349388cbb87
AntonMiranovich <antonmir992@gmail.com>
2023-12-13 21:30:19 +0300

    tasks123123123123

--- 062.OopTS/dist/app10.js
+++ 062.OopTS/dist/app10.js
@@ -2,3 +2,28 @@
 // price (цена продукта), title (название продукта). Реализуйте метод
 // displayProductInfo(), который выводит массив продуктов, где цена > 20. Создайте
 // экземпляр класса Product и вызовите метод displayProductInfo().
+class Product {
+    product = [
+        {
+            price: 25,
+            title: "Clock",
+        },
+        {
+            price: 29,
+            title: "Phone",
+        },
+        {
+            price: 10,
+            title: "Mouse",
+        },
+    ];
+    displayProductInfo() {
+        const arr10 = [];
+        this.product.forEach((el) => {
+            el.price > 20 ? arr10.push(el) : null;
+        });
+        return arr10;
+    }
+}
+const product = new Product();
+console.log(product.displayProductInfo());
--- 062.OopTS/src/app10.ts
+++ 062.OopTS/src/app10.ts
@@ -2,3 +2,38 @@
 // price (цена продукта), title (название продукта). Реализуйте метод
 // displayProductInfo(), который выводит массив продуктов, где цена > 20. Создайте
 // экземпляр класса Product и вызовите метод displayProductInfo().
+
+interface iProduct {
+  price: number;
+  title: string;
+}
+
+class Product {
+  product: iProduct[] = [
+    {
+      price: 25,
+      title: "Clock",
+    },
+    {
+      price: 29,
+      title: "Phone",
+    },
+    {
+      price: 10,
+      title: "Mouse",
+    },
+  ];
+
+  displayProductInfo() {
+    const arr10: iProduct[] = [];
+
+    this.product.forEach((el: iProduct) => {
+      el.price > 20 ? arr10.push(el) : null;
+    });
+
+    return arr10;
+  }
+}
+
+const product = new Product();
+console.log(product.displayProductInfo());
